d8ad5d0e3a1d6c2da092e1756d8a592d88398f86
m2en <me@m2en.dev>
2023-02-15 17:55:27 +0900

    docs: add doc comments

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,16 @@
 use dotenvy::dotenv;
 use std::env;
 
+/// Returns the value of the environment variable for the Key specified in the first argument.
+/// - If the `.env` file does not exist, an error is returned.
+/// - If the value does not exist, an error is raised and the Key for which the value does not exist is reported by cargo.
+///
+/// ### Example:
+/// ```
+/// async fn main() {
+///    println!("{}", get_env("HOSTNAME"))
+/// }
+/// ```
 pub fn get_env(key: &str) -> String {
     dotenv().expect(".env file not found");
     env::var(key).unwrap_or_else(|_| panic!("{key} must be set"))
