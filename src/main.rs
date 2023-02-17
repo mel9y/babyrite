6a968374e0c08111275196da8d4c6b0c69d6d2ba
m2en <me@m2en.dev>
2023-02-18 05:13:38 +0900

    chore: remove old login log

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::utils::get_env;
 use api::client::discord_client_builder;
 
 mod api;
+mod event;
 mod utils;
 
 #[tokio::main]
@@ -9,7 +10,6 @@ async fn main() {
     let mut client = discord_client_builder(get_env("DISCORD_BOT_TOKEN").as_str()).await;
     let task = client.start();
 
-    println!("Login succeeded");
     if let Err(why) = task.await {
         println!("An error occurred while running the client: {why:?}");
     }
