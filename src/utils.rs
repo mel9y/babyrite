dbf930a7b72a79db30f08b3f4559af1638fe686c
m2en <me@m2en.dev>
2023-02-22 18:06:28 +0900

    fix: remove env not found error

--- src/utils.rs
+++ src/utils.rs
@@ -12,6 +12,6 @@ use std::env;
 /// }
 /// ```
 pub fn get_env(key: &str) -> String {
-    dotenv().expect(".env file not found");
+    dotenv().ok();
     env::var(key).unwrap_or_else(|_| panic!("{key} must be set"))
 }
